a569e66e74963fff246261b3e0caa121c575274f
RubyUmbra <rubyumbra@gmail.com>
2020-10-14 13:59:34 +0300

    Routes configured

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -11,20 +11,23 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/timeline",
+    name: "Timeline",
+    component: () => import("../views/Timeline.vue")
+  },
+  {
+    path: "/auth",
+    name: "Auth",
+    component: () => import("../views/Auth.vue")
   }
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
+  linkActiveClass: "active",
+  linkExactActiveClass: "active"
 });
 
 export default router;
